939b8e8b64169f24f38a77574152d04a95bd4854
tomthomas123 <tomthomas1248@gmail.com>
2022-11-04 12:31:26 +0530

    menu added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int choice;
+        while (true)
+        {
+            System.out.println("select an option");
+            System.out.println("1.Add student");
+            System.out.println("2.Search student");
+            System.out.println("3.delete student");
+            System.out.println("4.view student");
+            System.out.println("5.Exit");
+            Scanner scanner=new Scanner(System.in);
+            choice=scanner.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("Add student selected");
+                    break;
+                case 2:
+                    System.out.println("search student selected");
+                    break;
+                case 3:
+                    System.out.println("delete student selected");
+                    break;
+                case 4:
+                    System.out.println("view student selected");
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
